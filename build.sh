972ef9b38727de61db3a3ec5d138a9f803cfb71d
Emmanuel Aforo <117290716+zeroruntime@users.noreply.github.com>
2025-06-21 02:07:27 +0000

    Changed script

--- build.sh
+++ build.sh
@@ -5,3 +5,5 @@ set -o errexit
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
+
+echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser('ktc', 'reddycore14@gmail.com', 'knightcore')" | python manage.py shell
