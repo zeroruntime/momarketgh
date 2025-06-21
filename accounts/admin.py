13775d83b87b8146358b96e11decc380f07f4054
Emmanuel Aforo <117290716+zeroruntime@users.noreply.github.com>
2025-06-21 02:16:20 +0000

    Added User in admin view

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
-
+from accounts.models import User  # Import the User model
 # Register your models here.
+admin.site.register(User)  # Register the User model with the admin site
